ee3bf6a5479077cd206c57a26a5e12833307200b
Thomas Bruce <71598048+thomasbr404@users.noreply.github.com>
2020-10-01 09:25:13 -0400

    Add ladder.cs and ladder.sln

---
+++ ladder.cs
@@ -0,0 +1,71 @@
+﻿// webserver
+// by Thomas Bruce
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ladder {
+    class ladder {
+        public static HttpListener listener;
+        public static string url = "http://localhost:8000/";
+        public static int pageViews = 0;
+        public static int requestCount = 0;
+        public static string pageData =
+            "<!DOCTYPE>" +
+            "<html>" +
+            "  <head>" +
+            "    <title>ladder web server</title>" +
+            "  </head>" +
+            "  <body>" +
+            "    <p>Page Views: {0}</p>" +
+            "  </body>" +
+            "</html>";
+
+        public static async Task HandleIncomingConnections() {
+            bool runServer = true;
+            while (runServer) {
+                HttpListenerContext ctx = await listener.GetContextAsync();
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine();
+
+                
+
+                if (req.Url.AbsolutePath != "/favicon.ico")
+                    pageViews += 1;
+
+                string disableSubmit = !runServer ? "disabled" : "";
+                byte[] data = Encoding.UTF8.GetBytes(String.Format(pageData, pageViews, disableSubmit));
+                resp.ContentType = "text/html";
+                resp.ContentEncoding = Encoding.UTF8;
+                resp.ContentLength64 = data.LongLength;
+
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
+
+            }
+        }
+
+        public static void Main(string[] args) {
+
+            listener = new HttpListener(); // Create a Http server and start listening for incoming connections
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            listener.Close();
+        }
+    }
+}
